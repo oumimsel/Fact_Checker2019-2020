a33fee8987f9905e0752df79553868ae893195f7
oumama <oumama.msellek@gmail.com>
2019-11-13 18:33:45 +0100

    Add opennlp to maven

--- Fact_checker/src/Checker.java
+++ Fact_checker/src/Checker.java
@@ -1,4 +1,10 @@
 
+import opennlp.tools.sentdetect.SentenceDetector;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+
 public class Checker {
+
+	
 
 }
